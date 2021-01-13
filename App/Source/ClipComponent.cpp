f42971babac4c26cc45f88b38f6b5dd13ab837cd
Steffen Baranowsky <BaraMGB@freenet.de>
2021-01-13 19:51:13 +0100

    use SmartThumbnail instead of own implementation

--- App/Source/ClipComponent.cpp
+++ App/Source/ClipComponent.cpp
@@ -151,42 +151,41 @@ void ClipComponent::showContextMenu()
 
 AudioClipComponent::AudioClipComponent (EditViewState& evs, te::Clip::Ptr c)
     : ClipComponent (evs, c)
-    , m_thumbnailComponent (evs)
 {
-    addAndMakeVisible (m_thumbnailComponent);
-    m_thumbnailComponent.setFile (getWaveAudioClip ()->getOriginalFile ());
+    updateThumbnail ();
 }
 
 void AudioClipComponent::paint (juce::Graphics& g)
 {
     ClipComponent::paint (g);
 
+    auto viewportOffset = -(m_editViewState.timeToX(0, getParentComponent()->getWidth()));
+
+    auto viewportEndX =  getParentComponent()->getWidth();
+    auto clipstartX = m_editViewState.timeToX(m_clip->getPosition().getStart(), getParentComponent()->getWidth());
+    auto clipendX = clipstartX + getWidth();
+
+    auto left = clipstartX < 0 ? -clipstartX : 0;
+    auto right = clipendX > viewportEndX ? clipendX - viewportEndX : 0;
+
+    if (m_editViewState.m_drawWaveforms && thumbnail != nullptr)
+    {
+        drawWaveform(g
+                     , *getWaveAudioClip()
+                     , *thumbnail
+                     , juce::Colours::black.withAlpha(0.5f)
+                     , left - viewportOffset
+                     , getWidth() - right - viewportOffset
+                     , 12
+                     , getHeight() - 14
+                     , viewportOffset);
+    }
+
 }
 
 void AudioClipComponent::resized()
 {
-    auto leftOffset = 0;
-    if(getBoundsInParent ().getX () < 0)
-    {
-        leftOffset = 0 - getBoundsInParent ().getX ();
-    }
 
-    auto rightOffset = 0;
-    if (getBoundsInParent().getRight() > getParentWidth())
-    {
-        rightOffset = getBoundsInParent().getRight() - getParentWidth();
-    }
-
-    m_thumbnailComponent.setDrawOffset (m_clip->getPosition ().offset);
-    auto drawOffsetTimeRight = getWaveAudioClip()->getSourceLength()
-            - m_clip->getPosition().getOffset()
-            - m_clip->getPosition().getLength();
-    m_thumbnailComponent.setDrawOffsetRight(drawOffsetTimeRight);
-    m_thumbnailComponent.setBounds (1 + leftOffset
-                                    ,11
-                                    , (getWidth() - 1 - leftOffset)
-                                        - rightOffset
-                                    , getHeight () - 11);
 }
 
 void AudioClipComponent::mouseMove(const juce::MouseEvent &e)
@@ -275,6 +274,120 @@ tracktion_engine::WaveAudioClip *AudioClipComponent::getWaveAudioClip()
 {
     return dynamic_cast<te::WaveAudioClip*> (m_clip.get());
 }
+
+void AudioClipComponent::drawWaveform(juce::Graphics& g,
+                                      te::AudioClipBase& c,
+                                      te::SmartThumbnail& thumb,
+                                      juce::Colour colour,
+                                       int left, int right, int y, int h, int xOffset)
+{
+    auto getTimeRangeForDrawing = [this] (const int left, const int right) -> te::EditTimeRange
+    {
+        if (auto p = getParentComponent())
+        {
+            double t1 = m_editViewState.xToTime (left, p->getWidth());
+            double t2 = m_editViewState.xToTime (right, p->getWidth());
+
+            return { t1, t2 };
+        }
+
+        return {};
+    };
+
+    jassert (left <= right);
+    const auto gain = c.getGain();
+    const auto pan = thumb.getNumChannels() == 1 ? 0.0f : c.getPan();
+
+    const float pv = pan * gain;
+    const float gainL = (gain - pv);
+    const float gainR = (gain + pv);
+
+    const bool usesTimeStretchedProxy = c.usesTimeStretchedProxy();
+
+    const auto clipPos = c.getPosition();
+    auto offset = clipPos.getOffset();
+    auto speedRatio = c.getSpeedRatio();
+
+    g.setColour (colour);
+
+    if (usesTimeStretchedProxy)
+    {
+        const juce::Rectangle<int> area(left + xOffset, y, right - left, h);
+
+        if (! thumb.isOutOfDate())
+        {
+            drawChannels (g, thumb, area, false,
+                          getTimeRangeForDrawing (left, right),
+                          c.isLeftChannelActive(), c.isRightChannelActive(),
+                          gainL, gainR);
+        }
+    }
+    else if (c.getLoopLength() == 0)
+    {
+        auto region = getTimeRangeForDrawing (left, right);
+
+        auto t1 = (region.getStart() + offset) * speedRatio;
+        auto t2 = (region.getEnd()   + offset) * speedRatio;
+        bool useHighres = true;
+        drawChannels (g, thumb,
+                      { left + xOffset, y, right - left, h },
+                      useHighres, { t1, t2 },
+                      c.isLeftChannelActive(), c.isRightChannelActive(),
+                      gainL, gainR);
+    }
+}
+
+void AudioClipComponent::drawChannels(juce::Graphics& g,
+                                      te::SmartThumbnail& thumb,
+                                      juce::Rectangle<int> area,
+                                      bool useHighRes,
+                                       te::EditTimeRange time, bool useLeft, bool useRight,
+                                       float leftGain, float rightGain)
+{
+    if (useLeft && useRight && thumb.getNumChannels() > 1)
+    {
+        thumb.drawChannel (g, area.removeFromTop (area.getHeight() / 2), useHighRes, time, 0, leftGain);
+        thumb.drawChannel (g, area, useHighRes, time, 1, rightGain);
+    }
+    else if (useLeft)
+    {
+        thumb.drawChannel (g, area, useHighRes, time, 0, leftGain);
+    }
+    else if (useRight)
+    {
+        thumb.drawChannel (g, area, useHighRes, time, 1, rightGain);
+    }
+}
+
+void AudioClipComponent::updateThumbnail()
+{
+    if (auto* wac = getWaveAudioClip())
+    {
+        te::AudioFile af (wac->getAudioFile());
+
+        if (af.getFile().existsAsFile() || (! wac->usesSourceFile()))
+        {
+            if (af.isValid())
+            {
+                const te::AudioFile proxy ((wac->hasAnyTakes() && wac->isShowingTakes()) ? wac->getAudioFile() : wac->getPlaybackFile());
+
+                if (thumbnail == nullptr)
+                {
+                    thumbnail = std::make_unique<te::SmartThumbnail>(wac->edit.engine, proxy, *this, &wac->edit);
+                }
+                else
+                {
+                    thumbnail->setNewFile (proxy);
+                }
+            }
+            else
+            {
+                thumbnail = nullptr;
+            }
+        }
+    }
+}
+
 
 //==============================================================================
 
