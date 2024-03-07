da230064681d52edbc15fd0c1a158b57d67ae3ed
Steffen Baranowsky <BaraMGB@arcor.de>
2024-03-08 00:25:29 +0100

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <build_type>"
+    echo "build_type: d for Debug, r for Release, rd for RelWithDebInfo"
+    exit 1
+fi
+
+case $1 in
+    d) BUILD_TYPE="Debug" ;;
+    r) BUILD_TYPE="Release" ;;
+    rd) BUILD_TYPE="RelWithDebInfo" ;;
+    *)
+        echo "Invalid build type: $1"
+        echo "Valid types are: d (Debug), r (Release), rd (RelWithDebInfo)"
+        exit 1
+        ;;
+esac
+
 mkdir -p ./build/
 cd build
-rm CMakeCache.txt
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo .. && cmake --build . && mv ~/Projects/NextSTudio/builds/compile_commands.json ~/Projects/NextSTudio/ && ./App/NextStudio_artefacts/RelWithDebInfo/NextStudio
+rm -f CMakeCache.txt
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && make -j6 && mv ~/Projects/NextSTudio/build/compile_commands.json ~/Projects/NextSTudio/ && ./App/NextStudio_artefacts/$BUILD_TYPE/NextStudio
 cd ..
+
