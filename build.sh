2ed2a1fab19b41037a14714328912bdbb2262459
Steffen Baranowsky <BaraMGB@arcor.de>
2024-03-08 13:23:59 +0100

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+echo "----------------------------------------------------------"
+echo "Starting build script..."
+# Ensure the script is running under Bash
+if [ -z "$BASH_VERSION" ]; then
+    echo "Error: This script needs to be run using Bash, but it seems to be running in a different shell."
+    exit 1
+fi
+
 # Check if the build type argument is provided and valid
 if [[ $# -eq 0 ]] || [[ "$1" != "d" && "$1" != "r" && "$1" != "rd" ]]; then
+    echo "Error: No build type specified or invalid build type."
     echo "Usage: $0 [build type]"
     echo "Build type:"
     echo "  d  - Debug"
     echo "  r  - Release"
     echo "  rd - RelWithDebInfo"
     exit 1
+else
+    echo "Build type provided: $1"
+echo "----------------------------------------------------------"
 fi
 
 # Set the build type based on the argument
@@ -23,29 +35,61 @@ case $1 in
         ;;
 esac
 
-# Create and enter the build directory
-mkdir -p ./build/
-cd build
+echo "Configured build type: $BUILD_TYPE"
+echo "----------------------------------------------------------"
 
+# Create and enter the build directory
+echo "Creating and entering the build directory..."
+echo "----------------------------------------------------------"
+            # mkdir -p ./build/
+            # cd build || exit
+BUILD_DIR=./build/$BUILD_TYPE
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 # Remove previous CMake cache
+echo "Removing previous CMake cache..."
+echo "----------------------------------------------------------"
 rm -f CMakeCache.txt
 
 # Configure CMake and build the project
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && make -j6
+echo "Configuring CMake and starting the build..."
+echo "----------------------------------------------------------"
+if cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../.. && make -j6; then
+    echo "Build succeeded."
+    echo "----------------------------------------------------------"
+else
+    echo "Build failed."
+    echo "----------------------------------------------------------"
+    exit 1
+fi
 
 # Move compile_commands.json if it exists
-if [ -f ~/Projects/NextStudio/builds/compile_commands.json ]; then
-    mv ~/Projects/NextStudio/builds/compile_commands.json ~/Projects/NextStudio/
+if [ -f compile_commands.json ]; then
+    echo "Moving compile_commands.json to the project root..."
+    echo "----------------------------------------------------------"
+    mv compile_commands.json ../../
+else
+    echo "compile_commands.json does not exist, skipping."
+    echo "----------------------------------------------------------"
 fi
 
 # Go back to the original directory
-cd ..
+cd ../.. || exit
 
 # Create start.sh script
+echo "Creating start.sh script..."
+echo "----------------------------------------------------------"
+
 cat << EOF > start.sh
 #!/bin/bash
-./build/App/NextStudio_artefacts/$BUILD_TYPE/NextStudio
+echo "Starting the application..."
+./build/$BUILD_TYPE/App/NextStudio_artefacts/$BUILD_TYPE/NextStudio
 EOF
 
 # Make start.sh executable
+echo "Making start.sh executable..."
+echo "----------------------------------------------------------"
 chmod +x start.sh
+
+echo "Build script completed."
+echo "----------------------------------------------------------"
