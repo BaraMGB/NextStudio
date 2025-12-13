361e4aa5eef937e93182b252c3ea2ee802e2553e
Steffen Baranowksy <BaraMGB@arcor.de>
2025-12-13 13:54:09 +0100

    redesign menu bar in LowerRange

--- App/Source/LowerRangeTabBar.h
+++ App/Source/LowerRangeTabBar.h
@@ -1,30 +1,36 @@
 /*
-  ==============================================================================
 
-    LowerRangeTabBar.h
-    Created: 7 Dec 2025 4:06:24pm
-    Author:  Steffen
+This file is part of NextStudio.
+Copyright (c) Steffen Baranowsky 2019-2025.
 
-  ==============================================================================
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+
+==============================================================================
 */
-
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "EditViewState.h"
+#include "MenuBar.h"
 
-//==============================================================================
-/*
-*/
-class LowerRangeTabBar  : public juce::Component,
-                            public juce::ValueTree::Listener
+class LowerRangeTabBar  : public MenuBar,
+                          public juce::ValueTree::Listener
 {
 public:
     LowerRangeTabBar(EditViewState& evs);
     ~LowerRangeTabBar() override;
 
-    void paint (juce::Graphics&) override;
-    void resized() override;
     void valueTreePropertyChanged(juce::ValueTree&, const juce::Identifier&) override;
 
     std::function<void(LowerRangeView)> onTabSelected;
@@ -33,9 +39,9 @@ private:
     void updateTabButtons();
 
     EditViewState& m_evs;
-    juce::TextButton m_mixerButton {"Mixer"};
-    juce::TextButton m_midiEditorButton {"MIDI Editor"};
-    juce::TextButton m_pluginsButton {"Plugins"};
+    juce::DrawableButton m_mixerButton;
+    juce::DrawableButton m_midiEditorButton;
+    juce::DrawableButton m_pluginsButton;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LowerRangeTabBar)
 };
