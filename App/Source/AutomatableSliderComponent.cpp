7e1b4a9c0df6a3f05ee87330170f6f07356be9fe
Steffen Baranowksy <BaraMGB@arcor.de>
2026-01-01 23:25:26 +0100

    fix: AutomatableParameterComponent, Value Label don't change on modulate or automate / clean up code

--- App/Source/AutomatableSliderComponent.cpp
+++ App/Source/AutomatableSliderComponent.cpp
@@ -31,12 +31,14 @@ AutomatableSliderComponent::AutomatableSliderComponent(const tracktion_engine::A
     setSliderStyle(juce::Slider::RotaryVerticalDrag);
     setTextBoxStyle(juce::Slider::NoTextBox, 0, 0, false);
     bindSliderToParameter();
+    m_automatableParameter->addListener(this);
     if (auto t = m_automatableParameter->getTrack())
         m_trackColour = t->getColour();
 }
 
 AutomatableSliderComponent::~AutomatableSliderComponent()
 {
+    m_automatableParameter->removeListener(this);
 }
 
 void AutomatableSliderComponent::mouseDown(const juce::MouseEvent &e)
@@ -139,3 +141,187 @@ void AutomatableSliderComponent::chooseAutomatableParameter (std::function<void(
         handleChosenParam(m_automatableParameter);
 }
 
+void AutomatableSliderComponent::curveHasChanged(te::AutomatableParameter&)
+{
+}
+
+void AutomatableSliderComponent::currentValueChanged(te::AutomatableParameter&)
+{
+    setValue(m_automatableParameter->getCurrentValue(), juce::dontSendNotification);
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+AutomatableParameterComponent::AutomatableParameterComponent(const te::AutomatableParameter::Ptr ap, juce::String name)
+    : m_automatableParameter (ap)
+{
+    m_knob = std::make_unique<AutomatableSliderComponent>(ap);
+    m_knob->setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    m_knob->setSliderStyle(juce::Slider::RotaryVerticalDrag);
+    m_knob->setDoubleClickReturnValue(true, ap->getCurrentValue());
+    m_knob->onValueChange = [this] {
+        updateLabel();
+    };
+
+    addAndMakeVisible(*m_knob);
+
+    m_valueLabel.setJustificationType(juce::Justification::centredTop);
+    m_valueLabel.setFont(juce::Font(juce::FontOptions{11.0f}));
+    updateLabel();
+    addAndMakeVisible(m_valueLabel);
+
+    m_titleLabel.setJustificationType(juce::Justification::centredBottom);
+    m_titleLabel.setFont(juce::Font(juce::FontOptions{11.0f}));
+    m_titleLabel.setText(name, juce::dontSendNotification);
+
+    addAndMakeVisible(m_titleLabel);
+
+    m_automatableParameter->addListener(this);
+}
+
+AutomatableParameterComponent::~AutomatableParameterComponent()
+{
+    m_automatableParameter->removeListener(this);
+}
+void AutomatableParameterComponent::resized() 
+{
+    auto area = getLocalBounds();
+    auto h = area.getHeight() / 4;
+
+    m_titleLabel.setBounds(area.removeFromTop(h));
+    m_knob->setBounds(area.removeFromTop(h * 2));
+    m_valueLabel.setBounds(area.removeFromTop(h));
+}
+
+void AutomatableParameterComponent::setKnobColour (juce::Colour colour)
+{
+    m_knob->setTrackColour(colour);
+    repaint();
+}
+
+void AutomatableParameterComponent::updateLabel()
+{
+    m_valueLabel.setText(m_automatableParameter->getCurrentValueAsString(), juce::NotificationType::dontSendNotification);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+NonAutomatableParameterComponent::NonAutomatableParameterComponent(juce::Value v, juce::String name, int rangeStart, int rangeEnd)
+{
+    m_knob.setRange(rangeStart, rangeEnd, 1); 
+    m_knob.getValueObject().referTo(v);
+    m_titleLabel.setText(name, juce::dontSendNotification);
+    m_knob.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    m_knob.setSliderStyle(juce::Slider::RotaryVerticalDrag);
+
+    m_knob.onValueChange = [this] { updateLabel(); };
+    m_valueLabel.setJustificationType(juce::Justification::centredTop);
+    m_valueLabel.setFont(juce::Font(juce::FontOptions{11.0f}));
+    updateLabel();
+
+    m_titleLabel.setJustificationType(juce::Justification::centredBottom);
+    m_titleLabel.setFont(juce::Font(juce::FontOptions{11.0f}));
+
+    Helpers::addAndMakeVisible(*this,{&m_titleLabel, &m_knob, &m_valueLabel});
+}
+
+void NonAutomatableParameterComponent::resized()
+{
+    auto area = getLocalBounds();
+    auto h = area.getHeight() / 4;
+
+    m_titleLabel.setBounds(area.removeFromTop(h));
+    m_knob.setBounds(area.removeFromTop(h * 2));
+    m_valueLabel.setBounds(area.removeFromTop(h));
+}
+
+void NonAutomatableParameterComponent::updateLabel()
+{
+    m_valueLabel.setText(juce::String(m_knob.getValue()), juce::dontSendNotification);
+}
+
+void NonAutomatableParameterComponent::setSliderRange(double start, double end, double interval)
+{
+    m_knob.setRange(start, end, interval);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+AutomatableComboBoxComponent::AutomatableComboBoxComponent(te::AutomatableParameter::Ptr ap)
+: m_automatableParameter(ap)
+{
+    ap->addListener(this);
+    auto range = ap->valueRange;
+    int index = 1;
+    float step = range.interval > 0.0f ? range.interval : 1.0f;
+    for (float v = range.start; v <= range.end + 0.001f; v += step)
+    {
+        juce::String text;
+        if (ap->hasLabels())
+            text = ap->getLabelForValue(v);
+
+        if (text.isEmpty())
+            text = ap->valueToString(v);
+
+        addItem(text, index++);
+    }
+
+    // Initial update
+    currentValueChanged(*ap);
+
+    onChange = [this]
+        {
+            int index = getSelectedId();
+            if (index > 0)
+            {
+                auto range = m_automatableParameter->valueRange;
+                float step = range.interval > 0.0f ? range.interval : 1.0f;
+                float newVal = range.start + ((float)(index - 1) * step);
+
+                if (m_automatableParameter->getCurrentValue() != newVal)
+                    m_automatableParameter->setParameter(newVal, juce::sendNotification);
+            }
+        };
+}
+AutomatableComboBoxComponent::~AutomatableComboBoxComponent()
+{
+    m_automatableParameter->removeListener(this);
+}
+
+void AutomatableComboBoxComponent::currentValueChanged(te::AutomatableParameter& p)
+{
+    auto range = p.valueRange;
+    float step = range.interval > 0.0f ? range.interval : 1.0f;
+    int id = (int)std::round((p.getCurrentValue() - range.start) / step) + 1;
+
+    if (getSelectedId() != id)
+        setSelectedId(id, juce::dontSendNotification);
+}
+
+void AutomatableComboBoxComponent::chooseAutomatableParameter (std::function<void(te::AutomatableParameter::Ptr)> handleChosenParam, std::function<void()> /*startLearnMode*/)
+{
+    handleChosenParam(m_automatableParameter);
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+AutomatableChoiceComponent::AutomatableChoiceComponent(te::AutomatableParameter::Ptr  ap, juce::String name)
+{
+    m_combo = std::make_unique<AutomatableComboBoxComponent>(ap);
+    addAndMakeVisible(*m_combo);
+
+    m_titleLabel.setJustificationType(juce::Justification::centred);
+    m_titleLabel.setFont(juce::Font(juce::FontOptions{11.0f}));
+    m_titleLabel.setText(name, juce::dontSendNotification);
+    addAndMakeVisible(m_titleLabel);
+}
+
+void AutomatableChoiceComponent::resized()
+{
+    auto area = getLocalBounds();
+    area.reduce(area.getWidth() / 5, 0);
+    auto labelHeight = 15;
+    auto h = 30;
+    m_titleLabel.setBounds(area.removeFromTop(labelHeight));
+    m_combo->setBounds(area.removeFromTop(h));
+}
